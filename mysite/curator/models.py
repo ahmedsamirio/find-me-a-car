2746db61a954944e400b38315b3a000776a83da5
ahmed <ahmedsamirio95@gmail.com>
2020-10-17 07:23:59 +0200

    updated data model to include different models for brand, model and year

--- mysite/curator/models.py
+++ mysite/curator/models.py
@@ -1,27 +1,54 @@
 from django.db import models
 
+class Brand(models.model):
+    name = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.name
+
+
+class Model(model.model):
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    name = model.CharField(max_length=80)
+
+    def __str__(self):
+        return self.name
+
+
+class Year(model.model):
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    model = models.ForeignKey(Model, on_delete=models.CASCADE)
+    name = model.CharField(max_length=80)
+
+    def __str__(self):
+        return self.name
+
+
 # Create your models here.
 class Ad(models.Model):
-    brand = models.CharField(max_length=200)
-    model = models.CharField(max_length=200)
-    gov = models.CharField(max_length=200)
-    city = models.CharField(max_length=200)
-    date = models.DateField()  
-    year = models.IntegerField()
-    kilos = models.CharField(max_length=200)
-    pay_type = models.CharField(max_length=200)
-    transmission = models.CharField(max_length=200)
-    cc = models.CharField(max_length=200, blank=True, null=True)
-    chasis = models.CharField(max_length=200, blank=True, null=True)
-    features = models.CharField(max_length=500, blank=True, null=True)
-    color = models.CharField(max_length=200, blank=True, null=True)
-    price = models.IntegerField()
-    url = models.URLField()
-    description = models.CharField(max_length=500, blank=True, null=True)
-    imgs = models.CharField(max_length=2000, blank=True, null=True)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    model = models.ForeignKey(Model, on_delete=models.CASCADE)
+    year = models.ForeignKey(Year, on_delete=models.CASCADE)
+    # gov = models.CharField(max_length=200)
+    # city = models.CharField(max_length=200)
+    # date = models.DateField()  
+    # kilos = models.CharField(max_length=200)
+    # pay_type = models.CharField(max_length=200)
+    # transmission = models.CharField(max_length=200)
+    # cc = models.CharField(max_length=200, blank=True, null=True)
+    # chasis = models.CharField(max_length=200, blank=True, null=True)
+    # features = models.CharField(max_length=500, blank=True, null=True)
+    # color = models.CharField(max_length=200, blank=True, null=True)
+    # price = models.IntegerField()
+    # url = models.URLField()
+    # description = models.CharField(max_length=500, blank=True, null=True)
+    # imgs = models.CharField(max_length=2000, blank=True, null=True)
 
     def __str__(self):
         return '{} - {} - {}'.format(self.brand,
                                     self.model,
                                     self.year)
+
+
+
     
